01686b958b218b6c4af5830e7aa4ff15cdca765f
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-22 09:34:08 +0900

    Add the logo

--- ping-single-column/src/App.svelte
+++ ping-single-column/src/App.svelte
@@ -1,1 +1,24 @@
-<div id="app" />
+<script lang="ts">
+    import Logo from "./components/Logo.svelte";
+    import Title from "./components/Title.svelte";
+</script>
+
+<div id="app">
+    <div id="container">
+        <Logo />
+        <Title />
+    </div>
+</div>
+
+<style>
+    #app {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    #container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
+</style>
