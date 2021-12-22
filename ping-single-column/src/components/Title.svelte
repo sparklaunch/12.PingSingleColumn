ba85e5ff6482e143d142ae46d07ddc7340ba0e1e
Jinwook Kim <redwoodnatlpark@gmail.com>
2021-12-22 09:37:06 +0900

    Add the title

--- ping-single-column/src/components/Title.svelte
+++ ping-single-column/src/components/Title.svelte
@@ -1,1 +1,24 @@
-<div id="title" />
+<div id="title">
+    <h1>We are launching <span class="emphasized">soon!</span></h1>
+    <p>Subscribe and get notified</p>
+</div>
+
+<style>
+    #title {
+        text-align: center;
+        margin-bottom: 20px;
+    }
+    #title > h1 {
+        font-weight: 300;
+        font-size: 48px;
+        color: rgb(148, 148, 148);
+        margin-bottom: 10px;
+    }
+    #title > p {
+        font-size: 18px;
+    }
+    .emphasized {
+        color: black;
+        font-weight: 700;
+    }
+</style>
